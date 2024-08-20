e646b23ce53b6be3535d374a4f57ce46330b9676
Angelica Bufano <abufano@tql.com>
2024-08-20 12:13:09 -0400

    updated products

--- src/products/ProductCard.tsx
+++ src/products/ProductCard.tsx
@@ -12,39 +12,36 @@ function ProductCard({ product, onRemove }: ProductCardProps) {
     <div>
       <article className="card p-4" style={{ width: "20rem" }} key={product.id}>
         <div className="d-flex justify-content-end">
-        <Dropdown.Toggle variant="link" bsPrefix="p-0">
-          <svg className=" m-2 text-primary" width={18} height={20} fill="currentColor">
+          <Dropdown align="end">
+            <Dropdown.Toggle variant="link" bsPrefix="p-0">
+              <svg className=" m-2 text-primary" width={18} height={20} fill="currentColor">
                 <use xlinkHref="../node_modules/bootstrap-icons/bootstrap-icons.svg#three-dots-vertical" />
               </svg>
-          </Dropdown.Toggle>
-          </div>
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href={`/products/edit/${product.id}`}>Edit</Dropdown.Item>
+              <Dropdown.Item
+                onClick={(event: SyntheticEvent) => {
+                  event.preventDefault();
+                  onRemove(product);
+                }}>
+                Delete
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
         <big> {product.name} </big>
-        
- 
+
         <small>
-          <div>${product.price}  / {product.unit} </div>
+          <div>
+            ${product.price} / {product.unit}{" "}
+          </div>
           <br />
           <div>
-          <div> {product.vendor?.name} </div>
-        <div className="badge text-bg-primary"> {product.partNbr} </div>
+            <div> {product.vendor?.name} </div>
+            <div className="badge text-bg-primary"> {product.partNbr} </div>
           </div>
-         
         </small>
-
-        <Dropdown align="end">
-         
-
-          <Dropdown.Menu>
-            <Dropdown.Item href={`/products/edit/${product.id}`}>Edit</Dropdown.Item>
-            <Dropdown.Item
-              onClick={(event: SyntheticEvent) => {
-                event.preventDefault();
-                onRemove(product);
-              }}>
-              Delete
-            </Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
       </article>
     </div>
   );
--- src/vendors/VendorCard.tsx
+++ src/vendors/VendorCard.tsx
@@ -9,46 +9,41 @@ interface VendorCardProps {
 
 function VendorCard({ vendor, onRemove }: VendorCardProps) {
   return (
-    
     <div>
-
       <article className="card p-4" style={{ width: "18rem" }} key={vendor.id}>
         <div className="d-flex justify-content-between">
           <div className="">
-        <strong className="card-title"> {vendor.name} </strong>
-
-       
-       
+            <strong className="card-title"> {vendor.name} </strong>
           </div>
           <Dropdown align="end">
-          <Dropdown.Toggle variant="link" bsPrefix="p-0">
-          <svg className=" m-2 text-primary" width={30} height={20} fill="currentColor">
+            <Dropdown.Toggle variant="link" bsPrefix="p-0">
+              <svg className=" m-2 text-primary" width={30} height={20} fill="currentColor">
                 <use xlinkHref="../node_modules/bootstrap-icons/bootstrap-icons.svg#three-dots-vertical" />
               </svg>
-          </Dropdown.Toggle>
+            </Dropdown.Toggle>
 
-          <Dropdown.Menu>
-            <Dropdown.Item href={`/vendors/edit/${vendor.id}`}>Edit</Dropdown.Item>
-            <Dropdown.Item
-              onClick={(event: SyntheticEvent) => {
-                event.preventDefault();
-                onRemove(vendor);
-              }}>
-              Delete
-            </Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-          </div>
-          <div className="badge text-bg-secondary" style={{width:"5rem"}}>{vendor.code}</div>
-          <small>
+            <Dropdown.Menu>
+              <Dropdown.Item href={`/vendors/edit/${vendor.id}`}>Edit</Dropdown.Item>
+              <Dropdown.Item
+                onClick={(event: SyntheticEvent) => {
+                  event.preventDefault();
+                  onRemove(vendor);
+                }}>
+                Delete
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+        <div className="badge text-bg-secondary" style={{ width: "5rem" }}>
+          {vendor.code}
+        </div>
+        <small>
           <div>{vendor.address}</div>
           <div>
             {vendor.city} {vendor.state}
           </div>
           {vendor.zip}
         </small>
-        
-
       </article>
     </div>
   );
